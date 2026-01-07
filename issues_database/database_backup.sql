9e6a4bf2b2840c9d592afdffe248e05d7c72ee11
Kavia AI <production@kavia.ai>
2026-01-07 11:34:08 +0000

    Auto-commit: session cg50277140 - github-issue-insights-dashboard-303395

---
+++ issues_database/database_backup.sql
@@ -0,0 +1,93 @@
+--
+-- PostgreSQL database dump
+--
+
+\restrict 7y5o9jfz7EYjSjIlL8rHZ5ps9eWBvNasgXbKkHcPGmQWt9uHL67mKgvhP2oDRB8
+
+-- Dumped from database version 16.11 (Ubuntu 16.11-0ubuntu0.24.04.1)
+-- Dumped by pg_dump version 16.11 (Ubuntu 16.11-0ubuntu0.24.04.1)
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+DROP DATABASE IF EXISTS myapp;
+--
+-- Name: myapp; Type: DATABASE; Schema: -; Owner: postgres
+--
+
+CREATE DATABASE myapp WITH TEMPLATE = template0 ENCODING = 'UTF8' LOCALE_PROVIDER = libc LOCALE = 'en_US.UTF-8';
+
+
+ALTER DATABASE myapp OWNER TO postgres;
+
+\unrestrict 7y5o9jfz7EYjSjIlL8rHZ5ps9eWBvNasgXbKkHcPGmQWt9uHL67mKgvhP2oDRB8
+\connect myapp
+\restrict 7y5o9jfz7EYjSjIlL8rHZ5ps9eWBvNasgXbKkHcPGmQWt9uHL67mKgvhP2oDRB8
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+--
+-- Name: DATABASE myapp; Type: ACL; Schema: -; Owner: postgres
+--
+
+GRANT ALL ON DATABASE myapp TO appuser;
+
+
+--
+-- Name: SCHEMA public; Type: ACL; Schema: -; Owner: pg_database_owner
+--
+
+GRANT ALL ON SCHEMA public TO appuser;
+
+
+--
+-- Name: DEFAULT PRIVILEGES FOR SEQUENCES; Type: DEFAULT ACL; Schema: public; Owner: postgres
+--
+
+ALTER DEFAULT PRIVILEGES FOR ROLE postgres IN SCHEMA public GRANT ALL ON SEQUENCES TO appuser;
+
+
+--
+-- Name: DEFAULT PRIVILEGES FOR TYPES; Type: DEFAULT ACL; Schema: public; Owner: postgres
+--
+
+ALTER DEFAULT PRIVILEGES FOR ROLE postgres IN SCHEMA public GRANT ALL ON TYPES TO appuser;
+
+
+--
+-- Name: DEFAULT PRIVILEGES FOR FUNCTIONS; Type: DEFAULT ACL; Schema: public; Owner: postgres
+--
+
+ALTER DEFAULT PRIVILEGES FOR ROLE postgres IN SCHEMA public GRANT ALL ON FUNCTIONS TO appuser;
+
+
+--
+-- Name: DEFAULT PRIVILEGES FOR TABLES; Type: DEFAULT ACL; Schema: public; Owner: postgres
+--
+
+ALTER DEFAULT PRIVILEGES FOR ROLE postgres IN SCHEMA public GRANT ALL ON TABLES TO appuser;
+
+
+--
+-- PostgreSQL database dump complete
+--
+
+\unrestrict 7y5o9jfz7EYjSjIlL8rHZ5ps9eWBvNasgXbKkHcPGmQWt9uHL67mKgvhP2oDRB8
+
